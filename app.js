0011f15c974160559fd9f56fadb953cc84a06b02
Prashant Srivastava <srprashant02@gmail.com>
2025-03-27 13:24:58 +0530

    Updated browser lists in package.json file

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom/client ";
-const parent=React.createElement("div",{id:"Parent"},React.createElement("div",{id:"child"},React.createElement("h1",{},"I am an H1 tag")))
+import ReactDOM from "react-dom/client";
+const parent=React.createElement("div",{id:"Parent"},React.createElement("div",{id:"child"},React.createElement("h1",{},"I am an H1 tag!!!")))
 
-const element = React.createElement("H1", {id:"heading"}, "Hello from React!!"); // element is nothing but JS objects in react(these are not HTML element).
+const element = React.createElement("H1", {id:"heading"}, "Hello from React!!!"); // element is nothing but JS objects in react(these are not HTML element).
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent); // it is basically create an element under root and render it(similiar to writing under root element in HTML  ex: <div id="root"> <h1>""xyz</h1></div>
