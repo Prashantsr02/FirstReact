634e2da9c0f7605f247029ef8ae4bf2c37774126
Prashant <srprashant02@gmail.com>
2025-04-23 14:27:17 +0530

    Shown different menu with help of dynamic routing for different resturants

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,28 +1,40 @@
 import { useState, useEffect } from "react";
-import {usePramas} from "react-router-dom";
-import {RES_URL} from "./utils/constant";
+import { useParams } from "react-router-dom";
+import { MENU_URL } from "../utils/constant";
+import Shimmer from "./Shimmer";
 
-const RestaurantMenu =()=>{
-    const[menu, setMenu]=useState();
-    const{resid}=usePramas();
-    useEffect(()=>{
-        fetchmenu();
-    }
-    ,[]);
+const RestaurantMenu = () => {
+  const [menu, setMenu] = useState(null);
+  const { resId } = useParams();
+  useEffect(() => {
+    fetchmenu();
+  }, []);
 
-    const fetchmenu=async()=>{
-        const data=await fetch(RES_URL+resid);
-        const jsondata= await data.json();
-    }
-    setMenu(jsondata);
-    
-    return (
-        <div>
-            <h1> {jsondata.name}</h1>
-            <h3> {jsondata.menu} </h3>  // have to map through all the menu items and show in UI.
-            <h3> {jsondata.costfortwo} </h3>
-        </div>
-    )
-}
+  const fetchmenu = async () => {
+    const data = await fetch(
+        MENU_URL+resId
+    );
+    const jsondata = await data.json();
+    console.log(jsondata);
+    setMenu(jsondata.data)
+  };
+  if(menu===null) return <Shimmer/>;
+  const {name,city} = menu?.cards[2]?.card?.card?.info;
+  const{itemCards} = menu?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
+  console.log(itemCards);
+  return (
+    <div className="menu">
+      <h1> {name}</h1>
+      <ul>
+        <li>
+        <h3>
+        { itemCards.map((item)=>{item.card.info.name}) }
+      </h3>
+        </li>
+      </ul>
+      <h3> {city} </h3>
+    </div>
+  );
+};
 
 export default RestaurantMenu;
